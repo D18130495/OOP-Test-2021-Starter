0a2b8178f2459df9df2ed32fb3a8ab5f14155d79
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-21 20:18:12 +0100

    write character above the notes

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -7,9 +7,9 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	String score = "DEFGABcd";
+	//String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	ArrayList<Note> notes = new ArrayList<Note>();
 
@@ -59,6 +59,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 + 50, 130 + i * gap + 10, height / 2);
 				circle(130 + i * gap, height / 2 + 50, 20);
 				if(n.getDuration() == 1) {
@@ -71,6 +73,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 + 40, 130 + i * gap + 10, height / 2 - 10);
 				circle(130 + i * gap, height / 2 + 40, 20);
 				if(n.getDuration() == 1) {
@@ -83,6 +87,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 + 30, 130 + i * gap + 10, height / 2 - 20);
 				circle(130 + i * gap, height / 2 + 40 - 10, 20);
 				if(n.getDuration() == 1) {
@@ -95,6 +101,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 + 20, 130 + i * gap + 10, height / 2 - 30);
 				circle(130 + i * gap, height / 2 + 40 - 20, 20);
 				if(n.getDuration() == 1) {
@@ -107,6 +115,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 + 10, 130 + i * gap + 10, height / 2 - 40);
 				circle(130 + i * gap, height / 2 + 40 - 30, 20);
 				if(n.getDuration() == 1) {
@@ -119,6 +129,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2, 130 + i * gap + 10, height / 2 - 50);
 				circle(130 + i * gap, height / 2 + 40 - 40, 20);
 				if(n.getDuration() == 1) {
@@ -131,6 +143,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 - 10, 130 + i * gap + 10, height / 2 - 60);
 				circle(130 + i * gap, height / 2 + 40 - 50, 20);
 				if(n.getDuration() == 1) {
@@ -143,6 +157,8 @@ public class ScoreDisplay extends PApplet
 					fill(255, 0, 0);
 					stroke(255, 0, 0);
 				}
+				text(n.getNote(), 130 + i * gap, 100);
+				textSize(25);
 				line(130 + i * gap + 10, height / 2 - 20, 130 + i * gap + 10, height / 2 - 70);
 				circle(130 + i * gap, height / 2 + 40 - 60, 20);
 				if(n.getDuration() == 1) {
