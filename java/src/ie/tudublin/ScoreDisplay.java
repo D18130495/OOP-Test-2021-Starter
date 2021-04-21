4f5f0491bfe71abbb19ffd26af65e4cd3e7241c6
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-21 19:23:12 +0100

    update draw note

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -54,27 +54,35 @@ public class ScoreDisplay extends PApplet
 			Note n = notes.get(i);
 			if(n.getNote() == 'D') {
 				fill(0);
-				circle(130 + i * gap, height / 2 + 40 + 10, 20);
+				line(130 + i * gap + 10, height / 2 + 50, 130 + i * gap + 10, height / 2);
+				circle(130 + i * gap, height / 2 + 50, 20);
 			} else if(n.getNote() == 'E') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 + 40, 130 + i * gap + 10, height / 2 - 10);
 				circle(130 + i * gap, height / 2 + 40, 20);
 			} else if(n.getNote() == 'F') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 + 30, 130 + i * gap + 10, height / 2 - 20);
 				circle(130 + i * gap, height / 2 + 40 - 10, 20);
 			} else if(n.getNote() == 'G') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 + 20, 130 + i * gap + 10, height / 2 - 30);
 				circle(130 + i * gap, height / 2 + 40 - 20, 20);
 			} else if(n.getNote() == 'A') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 + 10, 130 + i * gap + 10, height / 2 - 40);
 				circle(130 + i * gap, height / 2 + 40 - 30, 20);
 			} else if(n.getNote() == 'B') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2, 130 + i * gap + 10, height / 2 - 50);
 				circle(130 + i * gap, height / 2 + 40 - 40, 20);
 			} else if(n.getNote() == 'c') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 - 10, 130 + i * gap + 10, height / 2 - 60);
 				circle(130 + i * gap, height / 2 + 40 - 50, 20);
 			} else if(n.getNote() == 'd') {
 				fill(0);
+				line(130 + i * gap + 10, height / 2 - 20, 130 + i * gap + 10, height / 2 - 70);
 				circle(130 + i * gap, height / 2 + 40 - 60, 20);
 			}
 		}
