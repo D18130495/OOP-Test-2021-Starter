13d1c5d8d0a3357d55e6057ec0f2476edd437f99
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-21 19:12:59 +0100

    update stave lines and add draw notes function

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -7,7 +7,7 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	// String score = "DEFGABcd";
+	//String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
@@ -42,8 +42,48 @@ public class ScoreDisplay extends PApplet
 		for(int i = 0; i < 5; i++) {
 			stroke(0);
 			strokeWeight(3);
-			line(100, 200 + i * 25, width - 100, 200 + i * 25);
+			line(100, height / 2 - 40 + i * 20, width - 100, height / 2 - 40 + i * 20);
 		}
+	}
+
+	public void drawNotes()
+	{
+		float gap = (width - 260) / notes.size();
+
+		for(int i = 0; i < notes.size(); i++) {
+			Note n = notes.get(i);
+			if(n.getNote() == 'D') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 + 10, 20);
+			} else if(n.getNote() == 'E') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40, 20);
+			} else if(n.getNote() == 'F') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 10, 20);
+			} else if(n.getNote() == 'G') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 20, 20);
+			} else if(n.getNote() == 'A') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 30, 20);
+			} else if(n.getNote() == 'B') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 40, 20);
+			} else if(n.getNote() == 'c') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 50, 20);
+			} else if(n.getNote() == 'd') {
+				fill(0);
+				circle(130 + i * gap, height / 2 + 40 - 60, 20);
+			}
+		}
+
+		// for(int i = 0; i < notes.size(); i++) {
+		// 	float x = 130 + i * gap;
+		// 	float y = map();
+		// 	line(x, y, x, y - 50);
+		// }
 	}
 	
 	public void settings()
@@ -61,11 +101,6 @@ public class ScoreDisplay extends PApplet
 	{
 		background(255);
 		drawStaveLines();
-		
-	}
-
-	void drawNotes()
-	{
-		background(255);
+		drawNotes();
 	}
 }
